78e9103364d297a0711ebe3ce3b205bea7d6b668
Erik Gilling <konkers@konkers.net>
2009-06-05 17:24:31 -0700

    add lua to build system
    Signed-off-by: Erik Gilling <konkers@konkers.net>

---
+++ lib/lua/src/objects.mk
@@ -0,0 +1,5 @@
+LUA_CORE_OBJS=	lapi.o lcode.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o \
+	lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o  \
+	lundump.o lvm.o lzio.o
+LUA_LIB_OBJS=	lauxlib.o lbaselib.o ldblib.o liolib.o lmathlib.o loslib.o ltablib.o \
+	lstrlib.o loadlib.o linit.o
