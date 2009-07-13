66266246c34cfdd0d6b35f61a7c0478532addcb1
Erik Gilling <konkers@konkers.net>
2009-07-12 22:32:54 -0700

    add dpot work in progress
    Signed-off-by: Erik Gilling <konkers@konkers.net>

---
+++ avr/boards/dpot/main.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2009 Erik Gilling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <avr/io.h>
+
+#include <util.h>
+#include <proto.h>
+
+#include <flgmain.h>
+
+#include "pins.h"
+
+struct relay_cfg {
+	volatile uint8_t *port;
+	uint8_t pin;
+} __attribute__((packed));
+
+struct relay_cfg relays[] = {
+	{&PORTB, B_RELAY0},
+	{&PORTB, B_RELAY1},
+	{&PORTB, B_RELAY2},
+};
+
+void handle_relay(void *data, uint8_t idx, uint8_t state)
+{
+	if (state)
+		*relays[idx].port |= _BV(relays[idx].pin);
+	else
+		*relays[idx].port &= ~_BV(relays[idx].pin);
+}
+
+struct proto_widget widgets[] = {
+	PROTO_WIDGET_RELAY(0,10),
+	PROTO_WIDGET_RELAY(1,10),
+	PROTO_WIDGET_RELAY(2,10),
+};
+
+struct proto_handlers handlers = {
+	.relay = &handle_relay,
+};
+
+struct proto flg_proto = {
+	.handlers = &handlers,
+	.widgets = widgets,
+	.n_widgets = ARRAY_SIZE(widgets),
+};
+
+void flg_set_txen(uint8_t en)
+{
+	if (en)
+		PORTD |= _BV(D_TX_EN);
+	else
+		PORTD &= ~_BV(D_TX_EN);
+}
+
+void flg_hw_setup(void)
+{
+	DDRB =  0;
+
+	DDRC = 0;
+
+	DDRD = _BV(D_TX_EN) | _BV(D_DATA_LED);
+
+}
+
+static uint8_t data_led;
+void flg_recv(uint8_t c)
+{
+	PORTD |= _BV(D_DATA_LED);
+	data_led = 50;
+}
+
+
+void flg_ping(void)
+{
+	if (data_led)
+		data_led--;
+	else
+		PORTD &= ~_BV(D_DATA_LED);
+}
+
+void flg_work(void)
+{
+}
+
---
+++ avr/boards/dpot/pins.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009 Erik Gilling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* Port B */
+#define B_RELAY0	PB0
+#define B_RELAY1	PB1
+#define B_RELAY2	PB2
+/* unused		PB3 */
+/* unused		PB4 */
+/* unused		PB5 */
+#define B_XTAL1		PB6
+#define B_XTAL2		PB7
+
+/* Port C */
+/* unused		PC0 */
+/* unused		PC1 */
+/* unused		PC2 */
+/* unused		PC3 */
+/* unused		PC4 */
+/* unused		PC5 */
+#define C_RESET		PC6
+
+/* Port D */
+#define D_RXD		PD0
+#define D_TXD		PD1
+#define D_TX_EN		PD2
+#define D_DATA_LED	PD3
+/* unused		PD4 */
+/* unused		PD5 */
+/* unused		PD6 */
+/* unused		PD7 */
+
