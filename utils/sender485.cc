b200e6bfaccd692e2183fa27e5427eaced7f5bcc
Erik Gilling <konkers@konkers.net>
2009-07-12 22:32:07 -0700

    add get_adc command to sender485
    Signed-off-by: Erik Gilling <konkers@konkers.net>

--- utils/sender485.cc
+++ utils/sender485.cc
@@ -34,6 +34,9 @@ public:
 	virtual void resp(struct proto_packet *pkt) {
 		switch (pkt->cmd) {
 		case PROTO_CMD_GET_STATUS:
+		case PROTO_CMD_SWITCH_QUERY:
+		case PROTO_CMD_ADC_QUERY_LO:
+		case PROTO_CMD_ADC_QUERY_HI:
 			status = pkt->val;
 			done = true;
 			break;
@@ -48,7 +51,10 @@ public:
 	   "\n"
 	   "commands:\n"
 	   "    get_status <addr>\n"
+	   "    get_switch <addr>\n"
+	   "    get_adc <addr> <idx>\n"
 	   "    set_relay <addr> <val>\n"
+	   "    set_clear <addr> <val>\n"
 	   "    set_addr <addr> <new_addr>\n");
  }
 
@@ -98,6 +104,20 @@ int main(int argc, char *argv[])
 			printf( "setRelay(0x%02x, 0x%02x)\n", addr, val);
 			p.setRelay(addr, val);
 			argc -= 3;
+		} else if (!strcmp("clear_relay", argv[0])) {
+			if (argc < 3) {
+				usage();
+				return 1;
+			}
+
+			if (!get_uint8(argv[1], &addr) || !get_uint8(argv[2], &val)) {
+				usage();
+				return 1;
+			}
+
+			printf( "clearRelay(0x%02x, 0x%02x)\n", addr, val);
+			p.clearRelay(addr, val);
+			argc -= 3;
 		} else if (!strcmp("set_addr", argv[0])) {
 			if (argc < 3) {
 				usage();
@@ -123,7 +143,7 @@ int main(int argc, char *argv[])
 				return 1;
 			}
 
-			printf( "setStatus(0x%02x)", addr);
+			printf( "getStatus(0x%02x)", addr);
 			p.getStatus(addr);
 			h.resetDone();
 			while (!h.isDone()) {
@@ -131,6 +151,57 @@ int main(int argc, char *argv[])
 			}
 			printf(" = %02x\n", h.getStatus());
 			argc -= 2;
+		} else if (!strcmp("get_switch", argv[0])) {
+			if (argc < 2) {
+				usage();
+				return 1;
+			}
+
+			if (!get_uint8(argv[1], &addr)) {
+				usage();
+				return 1;
+			}
+
+			printf( "getSwitch(0x%02x)", addr);
+			p.getSwitch(addr);
+			h.resetDone();
+			while (!h.isDone()) {
+				p.waitForMsg(100);
+			}
+			printf(" = %02x\n", h.getStatus());
+			argc -= 2;
+		} else if (!strcmp("get_adc", argv[0])) {
+			unsigned resp;
+			if (argc < 3) {
+				usage();
+				return 1;
+			}
+
+			if (!get_uint8(argv[1], &addr)) {
+				usage();
+				return 1;
+			}
+
+			if (!get_uint8(argv[2], &val)) {
+				usage();
+				return 1;
+			}
+
+			printf( "getAdc(0x%02x, 0x%02x)", addr, val);
+			h.resetDone();
+			p.getAdcLo(addr, val);
+			while (!h.isDone()) {
+				p.waitForMsg(100);
+			}
+			resp = h.getStatus();
+			h.resetDone();
+			p.getAdcHi(addr, val);
+			while (!h.isDone()) {
+				p.waitForMsg(100);
+			}
+			resp |= h.getStatus() << 8;
+			printf(" %04x\n", resp);
+			argc -= 3;
 		} else {
 			usage();
 			return 1;
