d1c33fa87be0b9bdc244a9ca1c2d5e2277f1d599
Erik Gilling <konkers@konkers.net>
2009-08-18 23:14:48 -0700

    Add Thread class
    Signed-off-by: Erik Gilling <konkers@konkers.net>

---
+++ lib/cxx/Thread.cc
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2009 Erik Gilling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <Thread.hh>
+
+void *ThreadStart(void * data)
+{
+	Thread *t = (Thread *) data;
+	int rc;
+
+	t->running = true;
+	rc = t->run();
+	pthread_exit((void *) rc);
+}
+
+Thread::Thread()
+{
+	running = false;
+}
+
+Thread::~Thread()
+{
+	stop();
+}
+
+
+bool Thread::start(void)
+{
+	int ret;
+
+	if (!running) {
+		ret = pthread_create(&pthread, NULL, ThreadStart, this);
+		if (ret < 0) {
+			fprintf(stderr, "pthread_create failed: %s\n",
+				strerror(errno));
+			return false;
+		}
+	}
+	return true;
+}
+
+int Thread::stop(void)
+{
+	int rc = 0;
+	int ret;
+
+	if (running) {
+		ret = pthread_join(pthread, (void **)&rc);
+		if (ret < 0)
+			fprintf(stderr, "pthread_join failed: %s\n",
+				strerror(errno));
+		running = false;
+	}
+	return rc;
+}
+
---
+++ lib/cxx/include/Thread.hh
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2009 Erik Gilling
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __Thread_hh__
+#define __Thread_hh__
+
+#include <pthread.h>
+
+class Thread
+{
+public:
+	class Mutex
+	{
+	private:
+		pthread_mutex_t mutex;
+
+	public:
+		Mutex() {
+			pthread_mutex_init(&mutex, NULL);
+		}
+
+		~Mutex() {
+			pthread_mutex_destroy(&mutex);
+		}
+
+		void lock(void) {
+			pthread_mutex_lock(&mutex);
+		}
+
+		bool tryLock(void) {
+			int err;
+
+			err = pthread_mutex_trylock(&mutex);
+
+			return err < 0 ? false : true;
+		}
+
+		void unlock(void) {
+			pthread_mutex_unlock(&mutex);
+		}
+	};
+
+private:
+	bool running;
+	pthread_t pthread;
+
+	friend void *ThreadStart(void * data);
+
+protected:
+	virtual int run(void) = 0;
+
+public:
+	Thread();
+	virtual ~Thread();
+
+	bool start(void);
+	int stop(void);
+};
+
+
+#endif /* __Thread_hh__ */
