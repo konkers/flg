216d6a609197cabe9bc186c4dd28bd02a0b26ab7
Alex Graveley <alex@beatniksoftware.com>
2010-05-05 18:20:55 -0400

    Add -lpng to rules-host.mk.

--- rules-host.mk
+++ rules-host.mk
@@ -1,7 +1,7 @@
 OPT = -g -O2
 CFLAGS += ${OPT} -Wall -Werror -I${FLG_DIR}/lib/c/include
 CXXFLAGS += ${CFLAGS} -I${FLG_DIR}/lib/cxx/include -I${FLG_DIR}/lib/lua/src
-LIBS += -lflg
+LIBS += -lflg -lpng
 LIBSXX += -lflgxx -llua ${LIBS} 
 LDFLAGS += -L${FLG_DIR}/lib/c
 LDXXFLAGS += ${LDFLAGS} -L${FLG_DIR}/lib/cxx -L${FLG_DIR}/lib/lua/src 
